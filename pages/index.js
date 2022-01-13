4ac3ce5da79e9acb1fd46fe966249e4f8f0a761f
Renakhys <francesco.rizzi.1995@gmail.com>
2022-01-13 21:04:29 +0100

    first version of homepage

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,56 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import ParticleField from "react-particles-webgl";
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+  const config = {
+    showCube: false,
+    dimension: '2D',
+    velocity: 2,
+    boundaryType: 'bounce',
+    antialias: false,
+    direction: {
+      xMin: -1,
+      xMax: 1,
+      yMin: -1,
+      yMax: 1,
+      zMin: -1,
+      zMax: 1
+    },
+    lines: {
+      colorMode: 'rainbow',
+      color: '#351CCB',
+      transparency: 0.9,
+      limitConnections: true,
+      maxConnections: 20,
+      minDistance: 110,
+      visible: true
+    },
+    particles: {
+      colorMode: 'rainbow',
+      color: '#3FB568',
+      transparency: 0.9,
+      shape: 'circle',
+      boundingBox: 'canvas',
+      count: 330,
+      minSize: 20,
+      maxSize: 50,
+      visible: true
+    },
+    cameraControls: {
+      enabled: false,
+      enableDamping: true,
+      dampingFactor: 0.2,
+      enableZoom: true,
+      autoRotate: false,
+      autoRotateSpeed: 0.3,
+      resetCameraFlag: true
+    },
+    limitConnections: true
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,59 +59,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div className={styles.bg}>
+        <ParticleField config={config} />
+      </div>
+
       <main className={styles.main}>
+
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href='https://renakhys.duckdns.org'>Renakhys.duckdns.org</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Test server hosting
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
